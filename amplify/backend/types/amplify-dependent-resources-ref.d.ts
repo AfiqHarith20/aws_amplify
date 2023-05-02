1ccb9b7bc0bd41991a84a3d8c516b98edbc4abb7
Afiq Harith <afiqharith15@gmail.com>
2023-05-02 17:25:40 +0800

    Trying aws cloud

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "awsamplify": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "awsamplify809f8b9a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ lib/screens/entry.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+
+import '../amplifyconfiguration.dart';
+
+class EntryScreen extends StatefulWidget {
+  const EntryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EntryScreen> createState() => _EntryScreenState();
+}
+
+class _EntryScreenState extends State<EntryScreen> {
+  final amplify = Amplify;
+  late bool _amplifyConfigured = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    final auth = AmplifyAuthCognito();
+    final analytics = AmplifyAnalyticsPinpoint();
+
+    try {
+      amplify.addPlugin(auth);
+      amplify.addPlugin(analytics);
+
+      await amplify.configure(amplifyconfig);
+
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Text('Configure'),
+      ),
+    );
+  }
+}
